2d58df1bef88203d92ad4fc21fcb6db8615df8dc
Sergio González <sergiogonzalezmerino@gmail.com>
2024-08-14 18:31:41 +0200

    [ADD] Added favicon

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="shortcut icon" href="{{ asset('img/logo_recursos.svg') }}" type="image/x-icon">
 
     <title>{{ config('app.name', 'Recursos') }}</title>
 
